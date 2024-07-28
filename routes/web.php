630dd80c2a991eb7a0ce6333f2d1c3467e95dbcb
pradippathakchhetri <pradippathakchhetri@gmail.com>
2024-07-28 11:24:40 +0545

    web route / route set to index

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('royal-master.index.');                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+    return view('royal-master.index');                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
 });
 Route::get('/about', function () {
     return view('royal-master.about');
